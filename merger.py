0b22eb58c3b0b25b015f411ce2a5758f2d35396e
Patryk Nytko <pnytko@mggpaero.com>
2022-07-25 14:49:20 +0200

    fname manip

--- merger.py
+++ merger.py
@@ -1,9 +1,9 @@
-from os import listdir
-from os.path import isfile, join
 from PIL import Image
+import numpy as np
 from itertools import product
 import argparse
 import glob
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dir_in", type = str, required = True)
@@ -11,8 +11,15 @@ args = parser.parse_args()
 
 formatted_input = args.dir_in + '\*.*'
 
-def merge(dir_in):
-    list_dir = glob.glob(dir_in)
-    imgs = [Image.open(i) for i in list_dir]
-    imgs.show()
-merge(formatted_input)
+def load_images(img_path):
+    image_paths = glob.glob(img_path)
+    image_filenames = list(map(os.path.basename, image_paths))
+    image_splitted = list(map(os.path.splitext, image_filenames))
+    image_separated = []
+    for i in range(len(image_splitted)):
+        print(image_splitted[i][0])  
+    opened_images = [Image.open(i) for i in image_paths]
+
+load_images(formatted_input)
+
+#os.path.splitext
