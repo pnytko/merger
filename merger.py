34e40d353ab01eb9dedba071e68cb6e822fa2bd1
Patryk Nytko <pnytko@mggpaero.com>
2022-07-26 13:39:29 +0200

    prealpha

--- merger.py
+++ merger.py
@@ -5,6 +5,7 @@ import glob
 import os
 import re
 import itertools
+import warnings
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dir_in", type = str, required = True)
@@ -38,6 +39,7 @@ def regex_solution(word):
 def dictionary_fitting(k, v):
     #Dict structure - IMPORTANT
     dict = [{'path': k[i], 'corners': v[i]} for i in range(len(k))]
+
     return dict
 
 def dictionary_analysis(input_dict):
@@ -52,15 +54,27 @@ def dictionary_analysis(input_dict):
             if corners_list[i][0] == input_ele['corners'][0]:
                 group_paths[i].append(input_ele['path'])
 
-    # for group in group_paths:
-    #     print(group)
     return group_paths
+
+def merge_v_images(v_input):
+    h_imgs = []
+    for group in v_input:
+        opened_images = [Image.open(img) for img in group]
+        # opened_images = [Image.open(img) for i in range(len(v_input)) for img in v_input[i]]
+        imgs_comb = np.hstack( (np.asarray(img) for img in opened_images))
+        imgs_comb = Image.fromarray(imgs_comb)
+        h_imgs.append(imgs_comb)
+    out_image = np.vstack( (np.asarray(img) for img in h_imgs))
+    out_image = Image.fromarray(out_image)
+    out_image.save('out.jpg')
+
 
 #Multireturn defining - IMPORTANT
 image_separated, image_paths = load_images(formatted_input)
 c_value = regex_solution(image_separated)
 dict_to_analyse = dictionary_fitting(image_paths, c_value)
 grouped_analysis = dictionary_analysis(dict_to_analyse)
-print(grouped_analysis)
 
-#opened_images = [Image.open(j) for j in image_paths]
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    merge_v_images(grouped_analysis)
